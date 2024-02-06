059a34d4e2b9573064954fcaded9ac21f301ec77
Oleh Bohatov <obohatov@gmail.com>
2024-02-06 16:53:23 +0100

    comment my version of scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,29 +1,31 @@
+from typing import List
+
 from bs4 import BeautifulSoup
 import requests
 import time
 
 class BaseScraper:
-    def __init__(self, base_url, total_pages):
+    def __init__(self, base_url: str, total_pages: int) -> None:
         self.base_url = base_url
         self.total_pages = total_pages
         self.session = requests.Session()
 
-    def get_soup(self, url):
+    def get_soup(self, url: str) -> BeautifulSoup:
         response = self.session.get(url)
         return BeautifulSoup(response.text, "html.parser")
 
 
 class ImmoWebScraper(BaseScraper):
-    def get_page_url(self, page_number):
+    def get_page_url(self, page_number: int) -> str:
         return self.base_url + f"?countries=BE&page={page_number}&orderBy=relevance"
 
-    def get_properties_urls(self, page_url):
+    def get_properties_urls(self, page_url: str) -> List[str]:
         soup = self.get_soup(page_url)
         properties_urls = [a["href"] for a in soup.select("a.card__title-link")]
         time.sleep(1)
         return properties_urls
 
-    def get_all_properties_urls(self):
+    def get_all_properties_urls(self) -> List[str]:
         all_properties_urls = []
         for page_number in range(1, self.total_pages + 1):
             page_url = self.get_page_url(page_number)
@@ -36,9 +38,9 @@ base_url_apartment = "https://www.immoweb.be/en/search/apartment/for-sale"
 total_pages = 333
 
 scraper_house = ImmoWebScraper(base_url_house, total_pages)
-house_urls = scraper_house.get_all_properties_urls()
+house_urls = scraper_house.get_all_properties_urls()  # type: List[str]
 
 scraper_apartment = ImmoWebScraper(base_url_apartment, total_pages)
-apartment_urls = scraper_apartment.get_all_properties_urls()
+apartment_urls = scraper_apartment.get_all_properties_urls()  # type: List[str]
 
-all_properties_urls = house_urls + apartment_urls
+all_properties_urls = house_urls + apartment_urls  # type: List[str]
