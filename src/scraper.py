782818fe0073a6196c6e8cce995684def80a53f1
Léa <konincks.lea@gmail.com>
2024-02-08 11:29:41 +0100

    all good now

--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
 
 class PropertyScraper():
 
