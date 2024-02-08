09dbd8ebc03817d4cde16e7e1830a3fc71d42dbc
Oleh Bohatov <obohatov@gmail.com>
2024-02-08 12:09:29 +0100

    add concurrency to scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,10 @@
-from typing import List
-
-from bs4 import BeautifulSoup
+import threading
 import requests
 import time
+
+from typing import List
+from queue import Queue 
+from bs4 import BeautifulSoup
 
 
 class BaseScraper:
@@ -24,12 +26,38 @@ class ImmoWebScraper(BaseScraper):
         soup = self.get_soup(page_url)
         properties_urls = [a["href"] for a in soup.select("a.card__title-link")]
         return properties_urls
-
-    def get_all_properties_urls(self) -> List[str]:
-        all_properties_urls = []
-        for page_number in range(1, self.total_pages + 1):
+    
+    def worker(self):
+        while True:
+            page_number = self.q.get()
+            if page_number is None:
+                break
+ 
             page_url = self.get_page_url(page_number)
             properties_urls = self.get_properties_urls(page_url)
-            all_properties_urls.extend(properties_urls)
+            self.all_properties_urls.extend(properties_urls)
+            self.q.task_done()
             time.sleep(1)
-        return all_properties_urls    
+
+    def get_all_properties_urls(self) -> List[str]:
+        self.all_properties_urls = []
+        self.q = Queue()
+        threads = []
+        num_worker_threads = 10
+
+        for i in range(num_worker_threads):
+            t = threading.Thread(target=self.worker)
+            t.start()
+            threads.append(t)
+
+        for page_number in range(1, self.total_pages + 1):
+            self.q.put(page_number)
+
+        self.q.join()
+
+        for i in range(num_worker_threads):
+            self.q.put(None)
+        for t in threads:
+            t.join()
+
+        return self.all_properties_urls  
