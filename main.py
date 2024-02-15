d9edc416f410fee328d3b4288f5e3bf45b7fbc4b
jaggar Yussef <jaggaryussef@gmail.com>
2024-02-15 17:03:21 +0100

    meta data

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from concurrent.futures import ThreadPoolExecutor
 import json
 from src.LinkScraper import ImmoWebScraper
 from time import perf_counter
+from src.DataCleaner import clean_up
 
 base_url_house = "https://www.immoweb.be/en/search/house/for-sale"
 base_url_apartment = "https://www.immoweb.be/en/search/apartment/for-sale"
@@ -16,45 +17,51 @@ total_pages = 10
 
 start_time = perf_counter()
 
-scraper_house = ImmoWebScraper(base_url_house, total_pages)
-house_urls = scraper_house.get_all_properties_urls()
+# scraper_house = ImmoWebScraper(base_url_house, total_pages)
+# house_urls = scraper_house.get_all_properties_urls()
 
-scraper_apartment = ImmoWebScraper(base_url_apartment, total_pages)
-apartment_urls = scraper_apartment.get_all_properties_urls()
+# scraper_apartment = ImmoWebScraper(base_url_apartment, total_pages)
+# apartment_urls = scraper_apartment.get_all_properties_urls()
 
-all_properties_urls = house_urls + apartment_urls
+# all_properties_urls = house_urls + apartment_urls
 
-with open("properties_urls.pkl", "wb") as file:
-    pickle.dump(all_properties_urls, file)
+# with open("properties_urls.pkl", "wb") as file:
+#     pickle.dump(all_properties_urls, file)
 
-with open("properties_urls.csv", "w") as file:
-    writer = csv.writer(file)
-    for url in all_properties_urls:
-        writer.writerow([url])
+# with open("properties_urls.csv", "w") as file:
+#     writer = csv.writer(file)
+#     for url in all_properties_urls:
+#         writer.writerow([url])
 
-with open("properties_urls.json", "w") as file:
-    json.dump(all_properties_urls, file)
-
-
-######## PROPERTY SCRAPER ##########
-# outfile = 'property_data.csv'
+# with open("properties_urls.json", "w") as file:
+#     json.dump(all_properties_urls, file)
 
 
+# ######## PROPERTY SCRAPER ##########
+# # outfile = 'property_data.csv'
 
-# If the file exists then delete it - start with a blank state
-try:
-    os.remove('property_data.csv')
-except OSError:
-    pass
 
-# Instantiate the scraper object
-PropScraper = PropertyScraper()
 
-# Run the scraper with mulithreading map
-with ThreadPoolExecutor() as pool:
-    pool.map(PropScraper.scrape, all_properties_urls)
+# # If the file exists then delete it - start with a blank state
+# try:
+#     os.remove('property_data.csv')
+# except OSError:
+#     pass
 
-print(f"\nScraped {total_pages * 31} properties {perf_counter() - start_time} seconds.")
+# # Instantiate the scraper object
+# PropScraper = PropertyScraper()
+
+# # Run the scraper with mulithreading map
+# with ThreadPoolExecutor() as pool:
+#     pool.map(PropScraper.scrape, all_properties_urls)
+
+# print(f"\nScraped {total_pages * 31} properties {perf_counter() - start_time} seconds.")
+
+
+with open("property_data.csv", "r") as file:
+    data= csv.DictReader(file)
+    clean_up(data)
+
 
 def main():
     pass
---
+++ src/DataCleaner.py
@@ -0,0 +1,90 @@
+from urllib.parse import unquote  
+
+property_data = {
+        'ID' :                          0,
+        'Postal code' :                 0,
+        'Locality' :                    0,
+        'Type' :                        0,
+        'Subtype' :                     0,
+        'Price ' :                      0,
+        'Bedrooms' :                    0,
+        'Living area' :                 0,
+        'Kitchen Type' :                0,
+        'Furnished' :                   0,
+        'How many fireplaces?' :        0,
+        'Terrace surface' :             0,
+        'Garden surface' :              0,
+        'Surface of the plot' :         0,
+        'Number of frontages' :         0,  
+        'Swimming pool' :               0,
+        'Building condition' :          0,
+        'Surroundings type' :           0,
+        'Energy class' :                0,
+        'Building condition' :          0,
+        'Construction year' :           0,
+        'Number of frontages' :         0,
+        'Heating type' :                0,
+        'Primary energy consumption' :  0,
+        'Venue of the sale' :           0,
+        'Tenement building' :           0,
+        'New build' :                   0
+        } 
+
+def clean_up(data): 
+    line_count= 0
+
+    print(type(data))
+    for row in data:
+        if line_count == 0:
+            line_count += 1
+        else:
+       
+            # print(row["Locality"])  
+            # print(row["Subtype"])  
+            cleanse(row) 
+            analyse_data_quality(row)
+            # print(row)
+
+            line_count += 1 
+        # cleanse(row)
+    calculate_percentage(property_data, line_count)    
+    print(property_data)
+    
+
+
+def cleanse(row: dict):
+    locality = row["Locality"]
+    post_code= row["Postal code"]
+    # print(row["Subtype"])
+    #Decode encoded url values
+    if '%' in locality: 
+        # row["Locality"]= unquote(locality)
+        pass
+
+    if len(post_code) > 4: 
+        # row["Locality"]= unquote(locality)
+        # print(post_code)
+        # row= 0
+        # print(row)
+        pass
+
+    if row["Subtype"] == '':
+        # print("empty")
+        pass
+
+
+def analyse_data_quality(row: dict):
+
+    
+    for keyName in property_data.keys():
+        if row[keyName] == '' or row[keyName] == "Not specified" :
+            property_data[keyName] += 1
+
+    
+def calculate_percentage(data, line_count):
+    for key in  data.keys():
+        key= percentage(data[key], line_count)
+
+
+def percentage(part, whole):
+  return 100 * float(part)/float(whole)
